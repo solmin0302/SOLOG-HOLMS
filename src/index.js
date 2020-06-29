6651dda1c831b04d296049f034687f4cf62743c6
solmin <solgo123@gmail.com>
2020-06-29 18:47:52 +0900

    redux 기본 세팅 완료

--- src/index.js
+++ src/index.js
@@ -4,12 +4,20 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducers from './modules';
+
+const store = createStore(rootReducers, composeWithDevTools());
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );

68bbf2844969bb2d58aee1f13607100a380978d8
solmin <solgo123@gmail.com>
2020-07-02 16:34:38 +0900

    [2020.07.01] 쿼리 추출하여 페이징 기능 구현

--- src/api/posts/posts.ctrl.js
+++ src/api/posts/posts.ctrl.js
@@ -1,5 +1,6 @@
 import Post from '../../models/post';
 import mongoose from 'mongoose';
+import Joi from 'joi'; // 클라이언트가 request body 중 하나의 항목이라도 빼먹었을때는 400 오류를 발생해야합니다. if 문으로 따로 처리를 하는 방법이 있긴합니다만 이를 더 수월하게 해주는 JOI를 사용합니다.
 
 const { ObjectId } = mongoose.Types;
 
@@ -13,6 +14,19 @@ export const checkObjectId = (ctx, next) => {
 };
 
 export const write = async (ctx) => {
+  const schema = Joi.object().keys({
+    // 객체가 다음 필드를 가지고 있음을 검증
+    title: Joi.string().required(), // required() 가 있으면 필수 항복
+    body: Joi.string().required(),
+    tags: Joi.array().items(Joi.string()).required(), // 문자열로 이루어진 배열
+  });
+
+  const result = Joi.validate(ctx.request.body, schema);
+  if (result.error) {
+    ctx.status = 400; //bad request
+    ctx.body = result.error;
+    return;
+  }
   const { title, body, tags } = ctx.request.body;
 
   const post = new Post({
@@ -31,9 +45,25 @@ export const write = async (ctx) => {
 };
 
 export const list = async (ctx) => {
+  const page = parseInt(ctx.query.page || '1', 10);
+  if (page < 1) {
+    ctx.status = 400;
+    return;
+  }
   try {
-    const posts = await Post.find().exec();
-    ctx.body = posts;
+    const posts = await Post.find()
+      .sort({ _id: -1 })
+      .limit(10)
+      .skip((page - 1) * 10)
+      .lean()
+      .exec();
+    const postCount = await Post.countDocuments().exec();
+    ctx.set(`Last-Page`, Math.ceil(postCount / 10));
+    ctx.body = posts.map((post) => ({
+      ...post,
+      body:
+        post.body.length < 200 ? post.body : `${post.body.slice(0, 200)}...`,
+    }));
   } catch (e) {
     ctx.throw(500, e);
   }
@@ -65,6 +95,18 @@ export const remove = async (ctx) => {
 
 export const update = async (ctx) => {
   const { id } = ctx.params;
+  const schema = Joi.object().keys({
+    title: Joi.string(),
+    body: Joi.string(),
+    tags: Joi.array().items(Joi.string()),
+  });
+
+  const result = Joi.validate(ctx.request.body, schema);
+  if (result.error) {
+    ctx.status = 400;
+    ctx.body = result.error;
+    return;
+  }
   try {
     const post = await Post.findByIdAndUpdate(id, ctx.request.body, {
       new: true,

dda748614ac077134e16f915f5cb6a94ea7633c8
solmin <solgo123@gmail.com>
2020-07-13 20:06:29 +0900

    [2020.07.13] 회원가입 구현

--- client/src/containers/auth/RegisterForm.jsx
+++ client/src/containers/auth/RegisterForm.jsx
@@ -1,12 +1,14 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { changeField, initializeForm } from '../../modules/auth';
+import { changeField, initializeForm, register } from '../../modules/auth';
 import AuthForm from '../../components/auth/AuthForm';
 
 const RegisterForm = () => {
   const dispatch = useDispatch();
-  const { form } = useSelector(({ auth }) => ({
+  const { form, auth, authError } = useSelector(({ auth }) => ({
     form: auth.register,
+    auth: auth.auth,
+    authError: auth.authError,
   }));
 
   const onChange = (e) => {
@@ -22,11 +24,29 @@ const RegisterForm = () => {
 
   const onSubmit = (e) => {
     e.preventDefault();
+    const { username, password, passwordConfirm } = form;
+    if (password !== passwordConfirm) {
+      //error
+      return;
+    }
+    dispatch(register({ username, password }));
   };
 
   useEffect(() => {
     dispatch(initializeForm('register'));
   }, [dispatch]);
+
+  useEffect(() => {
+    if (authError) {
+      console.log('AUTHERROR');
+      console.log(authError);
+      return;
+    }
+    if (auth) {
+      console.log('register success');
+      console.log(auth);
+    }
+  }, [auth, authError]);
   return (
     <AuthForm
       type="register"

8141e5385423e6188a3aa08193d5be61e60223a0
solmin <solgo123@gmail.com>
2020-07-30 20:44:00 +0900

    [2020.07.30] pagination 구현

--- client/src/components/posts/Pagination.jsx
+++ client/src/components/posts/Pagination.jsx
@@ -10,3 +10,39 @@ const PaginationBlock = styled.div`
   justify-content: space-between;
   margin-bottom: 3rem;
 `;
+
+const PageNumber = styled.div``;
+
+const buildLink = ({ username, tag, page }) => {
+  parseInt(page, 10);
+  const query = qs.stringify({ tag, page });
+  return username ? `/@${username}?${query}` : `/?${query}`;
+};
+
+const Pagination = ({ page, lastPage, username, tag }) => {
+  return (
+    <PaginationBlock>
+      <Button
+        disabled={page === 1}
+        to={
+          page === 1 ? undefined : buildLink({ username, tag, page: page - 1 })
+        }
+      >
+        이전
+      </Button>
+      <PageNumber>{page}</PageNumber>
+      <Button
+        disabled={page === lastPage}
+        to={
+          page === lastPage
+            ? undefined
+            : buildLink({ username, tag, page: page + 1 })
+        }
+      >
+        다음
+      </Button>
+    </PaginationBlock>
+  );
+};
+
+export default Pagination;

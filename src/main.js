dacf1882c0f9e26e01c8007f15d095f0e40bde81
solmin <solgo123@gmail.com>
2020-08-03 12:44:47 +0900

    [2020.08.03] 프로젝트 빌드

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import Koa from 'koa';
 import Router from 'koa-router';
 import bodyParser from 'koa-bodyparser';
 import mongoose from 'mongoose';
+import serve from 'koa-static';
+import path from 'path';
+import send from 'koa-send';
 
 import api from './api';
 import jwtMiddleware from './lib/jwtMiddleware';
@@ -28,6 +31,18 @@ router.use('/api', api.routes());
 app.use(bodyParser());
 app.use(jwtMiddleware);
 app.use(router.routes()).use(router.allowedMethods());
+
+const buildDirectory = path.resolve(__dirname, '../client/build');
+console.log(buildDirectory);
+app.use(serve(buildDirectory));
+app.use(async (ctx) => {
+  //not found && 주소가 /api로 시작하지 않는 경우
+  if (ctx.status === 404 && ctx.path.indexOf('/api') !== 0)
+    [
+      //return index.html
+      await send(ctx, 'index.html', { root: buildDirectory }),
+    ];
+});
 
 const port = PORT || 4000;
 
